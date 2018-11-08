2c92c3e795a7f062695abee1984e9100ea46790a
Neha Pathak <neha.pathak@hotmail.com>
2018-11-08 16:13:49 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import config from './aws-exports'
+import Amplify from 'aws-amplify'
+Amplify.configure(config)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
